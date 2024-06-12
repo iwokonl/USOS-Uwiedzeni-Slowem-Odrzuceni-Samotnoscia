7b8864dc57465d44093a035326a3a88137a2ef70
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2024-06-12 14:33:54 +0200

    Create Spring boot app

---
+++ Spring boot/usos_uwm_new_ver/src/main/java/org/webleader/usos_uwm_new_ver/UsosUwmNewVerApplication.java
@@ -0,0 +1,13 @@
+package org.webleader.usos_uwm_new_ver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsosUwmNewVerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsosUwmNewVerApplication.class, args);
+    }
+
+}
---
+++ Spring boot/usos_uwm_new_ver/src/test/java/org/webleader/usos_uwm_new_ver/UsosUwmNewVerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webleader.usos_uwm_new_ver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsosUwmNewVerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
